991187e283bcae30c36ec73219b49816b1639b86
Squoll <stas.pudov@gmail.com>
2023-10-03 18:24:06 +0300

    Update GrammarChecker.js

--- GrammarChecker.js
+++ GrammarChecker.js
@@ -1,39 +1,46 @@
-let story =
-  'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
+let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
 
-let storyWords = story.split(" ");
-let unnecessaryWord = "literally";
-let misspelledWord = "beautifull";
-let badWord = "freaking";
-
-//console.log(storyWords);
-
+let storyWords = story.split(' ');
+let unnecessaryWord = 'literally';
+let misspelledWord = 'beautifull';
+let badWord = 'freaking';
+//console.log(storyWords)
 let count = 0;
 storyWords.forEach((word) => {
-  count++;
-  //console.log("count: " + count);
+count++;
 });
 storyWords = storyWords.filter((word) => {
-  return word !== unnecessaryWord;
+  if (word !== unnecessaryWord) {
+    return word;
+  }
 });
 storyWords = storyWords.map((word) => {
-  return word === misspelledWord ? "beautiful" : word;
+  if (word === misspelledWord) {
+    return 'beautiful';
+  } else {
+    return word;
+  }
+}
+
+);
+const badWordIndex = storyWords.findIndex((word) => {
+  if (word === badWord) {
+    return word;
+      }
 });
-let badWordIndex = storyWords.findIndex((word) => {
-  return word === badWord;
+storyWords[badWordIndex] = 'really';
+let lengthCheck = storyWords.every((word) => {
+return word.length > 10;
 });
-//console.log(badWordIndex);
-storyWords[78] = "really";
-const lengthCheck = storyWords.every((word) => {
-  return word.length < 10;
+const longWord = storyWords.findIndex((word) => {
+  if (word.length > 10) {
+    return word;
+  }
 });
-let lengthyWordIndex = storyWords.findIndex((word) => {
-  return word.length > 10;
-});
-storyWords[lengthyWordIndex] = "stunning";
-storyWords = storyWords.filter((word) => {
-  return word !== "very";
-});
+storyWords[longWord] = 'stunning';
+storyWords = storyWords.filter((word) => { return word !== 'very'});
+//console.log(storyWords[111]);
+//console.log(`Long word index is:  ${longWord}`);
 storyWords = storyWords.map((word) => {
   return word === "GW" ? "George Washington" : word;
 });
